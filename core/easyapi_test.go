6b5b661440ab56b7f753a87a5d1c54f7c3c26384
Muiz <muiz.dev@gmail.com>
2024-01-22 22:29:13 -0800

    Make test creation modular so that different data providers can be inserted to assert the expected behavior

--- core/easyapi_test.go
+++ core/easyapi_test.go
@@ -75,226 +75,231 @@ func GetRoute (res *Result, route_path string) *RouteResult {
 	return nil
 }
 
-func TestNormalConfig (t *testing.T) {
+func SetupDataProviderTests (t *testing.T, data_provider_creator func(payload []map[string]interface{})*DataProvider) {
 
-	payload := GenerateTestUserPayload(10)
-	test_user_provider := CreateTestableUserProvider(payload)
+	t.Run("fetch all", func (t *testing.T) {
 
-	config := &Config{
-		Schemas: []*Schema{
-			{ 
-				Name: "Users",
-				Provider: test_user_provider,
+		payload := GenerateTestUserPayload(10)
+		test_user_provider := data_provider_creator(payload)
+
+		config := &Config{
+			Schemas: []*Schema{
+				{ 
+					Name: "Users",
+					Provider: test_user_provider,
+				},
 			},
-		},
-	}
+		}
 
-	res, err := EasyApiImpl(config);
-	assert.NoError(t, err, "Default config failed.");
-	assert.GreaterOrEqual(t, len(res.Routes), 1)
+		res, err := EasyApiImpl(config);
+		assert.NoError(t, err, "Default config failed.");
+		assert.GreaterOrEqual(t, len(res.Routes), 1)
 
-	all_route := GetRoute(res, "/api/users/all")
-	assert.NotNil(t, all_route)
-	assert.Equal(t, all_route.Type, RequestType_GET)
+		all_route := GetRoute(res, "/api/users/all")
+		assert.NotNil(t, all_route)
+		assert.Equal(t, all_route.Type, RequestType_GET)
+
+	});
+
+	t.Run("setup api root", func (t *testing.T) {
+		payload := GenerateTestUserPayload(10)
+		test_user_provider := data_provider_creator(payload)
+
+		config := &Config{
+			Root: "/home",
+			Schemas: []*Schema{
+				{ 
+					Name: "Users",
+					Provider: test_user_provider,
+				},
+			},
+		}
+		res, err := EasyApiImpl(config);
+		assert.NoError(t, err, "Default config failed.");
+		assert.NotEmpty(t, res.Routes)
+		all_route := GetRoute(res, "/home/users/all")
+		assert.NotNil(t, all_route)
+	});
+
+	t.Run("invalid api root", func (t *testing.T) {
+		payload := GenerateTestUserPayload(10)
+		test_user_provider := data_provider_creator(payload)
+		config := &Config{
+			Root: "home",
+			Schemas: []*Schema{
+				{ 
+					Name: "Users",
+					Provider: test_user_provider,
+				},
+			},
+		}
+		_, err := EasyApiImpl(config);
+		assert.Error(t, err, "Default config failed.");
+		assert.ErrorContains(t, err, "root must begin with a forward slash")
+	});
+
+	t.Run("fetch all", func (t *testing.T) {
+		var kDataSize int = 10
+		payload := GenerateTestUserPayload(kDataSize)
+		test_user_provider := data_provider_creator(payload)
+
+		config := &Config{
+			Schemas: []*Schema{
+				{ 
+					Name: "Users",
+					Provider: test_user_provider,
+				},
+			},
+		}
+
+		res, err := EasyApiImpl(config);
+		assert.NoError(t, err, "Default config failed.")
+		assert.GreaterOrEqual(t, len(res.Routes), 1)
+
+		all_route := GetRoute(res, "/api/users/all")
+		assert.NotNil(t, all_route)
+
+		res_opaque, err := all_route.Action("")
+		assert.NoError(t, err)
+
+		data, ok := res_opaque.(*[]map[string]interface{})
+		assert.True(t, ok)
+		assert.NotNil(t, data)
+		assert.Equal(t, len(*data), kDataSize)
+	});
+
+	t.Run("fetch all bounded", func (t *testing.T) {
+		var kDataSize int = 10
+		payload := GenerateTestUserPayload(kDataSize)
+		test_user_provider := data_provider_creator(payload)
+
+		config := &Config{
+			Schemas: []*Schema{
+				{ 
+					Name: "Users",
+					Provider: test_user_provider,
+				},
+			},
+		}
+
+		res, err := EasyApiImpl(config);
+		assert.NoError(t, err, "Default config failed.")
+		assert.NotEmpty(t, res.Routes)
+		
+		all_route := GetRoute(res, "/api/users/all")
+		assert.NotNil(t, all_route)
+		var kNbEntriesToRetrieve = 4
+		res_opaque, err := all_route.Action(fmt.Sprintf("offset=2&&count=%d", kNbEntriesToRetrieve))
+		assert.NoError(t, err)
+
+		data, ok := res_opaque.(*[]map[string]interface{})
+		assert.True(t, ok)
+		assert.NotNil(t, data)
+		assert.Equal(t, kNbEntriesToRetrieve, len(*data))
+	});
+
+	t.Run("fetch all bounded 2", func (t *testing.T) {
+		var kDataSize int = 10
+		payload := GenerateTestUserPayload(kDataSize)
+		test_user_provider := data_provider_creator(payload)
+
+		config := &Config{
+			Schemas: []*Schema{
+				{ 
+					Name: "Users",
+					Provider: test_user_provider,
+				},
+			},
+		}
+
+		res, err := EasyApiImpl(config);
+		assert.NoError(t, err, "Default config failed.")
+		assert.NotEmpty(t, res.Routes)
+
+		all_route := GetRoute(res, "/api/users/all")
+		assert.NotNil(t, all_route)
+
+		var kNbEntriesToRetrieve = 3
+		res_opaque, err := all_route.Action(fmt.Sprintf("offset=8&&count=%d", kNbEntriesToRetrieve))
+		assert.NoError(t, err)
+
+		data, ok := res_opaque.(*[]map[string]interface{})
+		assert.True(t, ok)
+		assert.NotNil(t, data)
+		assert.Equal(t, kNbEntriesToRetrieve - 1, len(*data))
+	});
+
+	t.Run("fetch all bounded negative fails", func (t *testing.T) {
+		var kDataSize int = 10
+		payload := GenerateTestUserPayload(kDataSize)
+		test_user_provider := data_provider_creator(payload)
+
+		config := &Config{
+			Schemas: []*Schema{
+				{ 
+					Name: "Users",
+					Provider: test_user_provider,
+				},
+			},
+		}
+
+		res, err := EasyApiImpl(config);
+		assert.NoError(t, err, "Default config failed.")
+		assert.NotEmpty(t, res.Routes)
+
+		all_route := GetRoute(res, "/api/users/all")
+		assert.NotNil(t, all_route)
+
+		res_opaque, err := all_route.Action("offset=-1&&count=-1" )
+		assert.Error(t, err)
+		assert.Nil(t, res_opaque)
+	});
+
+	t.Run("find one result", func (t *testing.T) {
+		payload := []map[string]interface{}{
+			{
+				"name": "John",
+				"location": "Arizona",
+			}, {
+				"name": "Jimmy",
+				"location": "California",
+			},{
+				"name": "Alex",
+				"location": "Texas",
+			}, 
+		}
+
+		test_user_provider := data_provider_creator(payload)
+
+		config := &Config{
+			Schemas: []*Schema{
+				{ 
+					Name: "Users",
+					Provider: test_user_provider,
+				},
+			},
+		}
+
+		res, err := EasyApiImpl(config);
+		assert.NoError(t, err, "Default config failed.")
+		assert.NotEmpty(t, res.Routes)
+
+		all_route := GetRoute(res, "/api/users/findone")
+		assert.NotNil(t, all_route)
+
+		res_opaque, err := all_route.Action("name=\"-eq John\"&&location=\"-eq Arizona\"")
+		assert.NoError(t, err)
+
+		data, ok := res_opaque.(*map[string]interface{})
+		assert.True(t, ok)
+		assert.NotNil(t, data)
+		assert.Equal(t, (*data)["name"], "John")
+		assert.Equal(t, (*data)["location"], "Arizona")
+	});
 }
 
-func TestApiRootConfig (t *testing.T) {
-	payload := GenerateTestUserPayload(10)
-	test_user_provider := CreateTestableUserProvider(payload)
-
-	config := &Config{
-		Root: "/home",
-		Schemas: []*Schema{
-			{ 
-				Name: "Users",
-				Provider: test_user_provider,
-			},
-		},
-	}
-	res, err := EasyApiImpl(config);
-	assert.NoError(t, err, "Default config failed.");
-	assert.NotEmpty(t, res.Routes)
-	all_route := GetRoute(res, "/home/users/all")
-	assert.NotNil(t, all_route)
-}
-
-func TestApiInvalidRootConfig (t *testing.T) {
-	payload := GenerateTestUserPayload(10)
-	test_user_provider := CreateTestableUserProvider(payload)
-	config := &Config{
-		Root: "home",
-		Schemas: []*Schema{
-			{ 
-				Name: "Users",
-				Provider: test_user_provider,
-			},
-		},
-	}
-	_, err := EasyApiImpl(config);
-	assert.Error(t, err, "Default config failed.");
-	assert.ErrorContains(t, err, "root must begin with a forward slash")
-}
-
-func TestFetchAll (t *testing.T) {
-
-	var kDataSize int = 10
-	payload := GenerateTestUserPayload(kDataSize)
-	test_user_provider := CreateTestableUserProvider(payload)
-
-	config := &Config{
-		Schemas: []*Schema{
-			{ 
-				Name: "Users",
-				Provider: test_user_provider,
-			},
-		},
-	}
-
-	res, err := EasyApiImpl(config);
-	assert.NoError(t, err, "Default config failed.")
-	assert.GreaterOrEqual(t, len(res.Routes), 1)
-
-	all_route := GetRoute(res, "/api/users/all")
-	assert.NotNil(t, all_route)
-
-	res_opaque, err := all_route.Action("")
-	assert.NoError(t, err)
-
-	data, ok := res_opaque.(*[]map[string]interface{})
-	assert.True(t, ok)
-	assert.NotNil(t, data)
-	assert.Equal(t, len(*data), kDataSize)
-}
-
-func TestFetchAllBounded (t *testing.T) {
-
-	var kDataSize int = 10
-	payload := GenerateTestUserPayload(kDataSize)
-	test_user_provider := CreateTestableUserProvider(payload)
-
-	config := &Config{
-		Schemas: []*Schema{
-			{ 
-				Name: "Users",
-				Provider: test_user_provider,
-			},
-		},
-	}
-
-	res, err := EasyApiImpl(config);
-	assert.NoError(t, err, "Default config failed.")
-	assert.NotEmpty(t, res.Routes)
-	
-	all_route := GetRoute(res, "/api/users/all")
-	assert.NotNil(t, all_route)
-	var kNbEntriesToRetrieve = 4
-	res_opaque, err := all_route.Action(fmt.Sprintf("offset=2&&count=%d", kNbEntriesToRetrieve))
-	assert.NoError(t, err)
-
-	data, ok := res_opaque.(*[]map[string]interface{})
-	assert.True(t, ok)
-	assert.NotNil(t, data)
-	assert.Equal(t, kNbEntriesToRetrieve, len(*data))
-}
-
-func TestFetchAllBounded2 (t *testing.T) {
-
-	var kDataSize int = 10
-	payload := GenerateTestUserPayload(kDataSize)
-	test_user_provider := CreateTestableUserProvider(payload)
-
-	config := &Config{
-		Schemas: []*Schema{
-			{ 
-				Name: "Users",
-				Provider: test_user_provider,
-			},
-		},
-	}
-
-	res, err := EasyApiImpl(config);
-	assert.NoError(t, err, "Default config failed.")
-	assert.NotEmpty(t, res.Routes)
-
-	all_route := GetRoute(res, "/api/users/all")
-	assert.NotNil(t, all_route)
-
-	var kNbEntriesToRetrieve = 3
-	res_opaque, err := all_route.Action(fmt.Sprintf("offset=8&&count=%d", kNbEntriesToRetrieve))
-	assert.NoError(t, err)
-
-	data, ok := res_opaque.(*[]map[string]interface{})
-	assert.True(t, ok)
-	assert.NotNil(t, data)
-	assert.Equal(t, kNbEntriesToRetrieve - 1, len(*data))
-}
-
-func TestFetchAllBoundedNegative (t *testing.T) {
-
-	var kDataSize int = 10
-	payload := GenerateTestUserPayload(kDataSize)
-	test_user_provider := CreateTestableUserProvider(payload)
-
-	config := &Config{
-		Schemas: []*Schema{
-			{ 
-				Name: "Users",
-				Provider: test_user_provider,
-			},
-		},
-	}
-
-	res, err := EasyApiImpl(config);
-	assert.NoError(t, err, "Default config failed.")
-	assert.NotEmpty(t, res.Routes)
-
-	all_route := GetRoute(res, "/api/users/all")
-	assert.NotNil(t, all_route)
-
-	res_opaque, err := all_route.Action("offset=-1&&count=-1" )
-	assert.Error(t, err)
-	assert.Nil(t, res_opaque)
-}
-
-func TestFindOne (t *testing.T) {
-
-	payload := []map[string]interface{}{
-		{
-			"name": "John",
-			"location": "Arizona",
-		}, {
-			"name": "Jimmy",
-			"location": "California",
-		},{
-			"name": "Alex",
-			"location": "Texas",
-		}, 
-	}
-
-	test_user_provider := CreateTestableUserProvider(payload)
-
-	config := &Config{
-		Schemas: []*Schema{
-			{ 
-				Name: "Users",
-				Provider: test_user_provider,
-			},
-		},
-	}
-
-	res, err := EasyApiImpl(config);
-	assert.NoError(t, err, "Default config failed.")
-	assert.NotEmpty(t, res.Routes)
-
-	all_route := GetRoute(res, "/api/users/findone")
-	assert.NotNil(t, all_route)
-
-	res_opaque, err := all_route.Action("name=\"-eq John\"&&location=\"-eq Arizona\"")
-	assert.NoError(t, err)
-
-	data, ok := res_opaque.(*map[string]interface{})
-	assert.True(t, ok)
-	assert.NotNil(t, data)
-	assert.Equal(t, (*data)["name"], "John")
-	assert.Equal(t, (*data)["location"], "Arizona")
+func TestDataProviderTestableProvider (t *testing.T) {
+	SetupDataProviderTests(t, func(payload []map[string]interface{})*DataProvider {
+		return CreateTestableUserProvider(payload);
+	});
 }
